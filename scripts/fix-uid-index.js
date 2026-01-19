a3c40dfdfb3ca0323edafe21e111f17edd1f319c
Tarun516 <taruncv516@gmail.com>
2026-01-19 12:16:59 +0530

    changes

--- scripts/fix-uid-index.js
+++ scripts/fix-uid-index.js
@@ -7,7 +7,7 @@
 
 const mongoose = require('mongoose');
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://user:user@cluster0.tfvlujk.mongodb.net/extrahand?retryWrites=true&w=majority&appName=Cluster0';
+const MONGODB_URI = process.env.MONGODB_URI;
 
 (async () => {
   try {
